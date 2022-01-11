64a3d0f7a90a9b0ecf392acc941dec2fe242310c
hakanderkan <90130889+hakanderkan@users.noreply.github.com>
2022-01-11 19:27:31 +0300

    ödev4.sql

---
+++ ödev4.sql
@@ -0,0 +1,13 @@
+ödev 1-) select DISTINCT REPLACEMENT_COST from film ;
+
+ödev 2-)select count(DISTINCT REPLACEMENT_COST) from film ;
+
+ödev3-) select count(*) from film 
+where title like 'T%' and rating = 'G' ;
+
+ödev4-)select count(*) from country 
+where country like '_____';
+
+
+ödev5-) select count(*) from city 
+where city Ilike '%r' ;
